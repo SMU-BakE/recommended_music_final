ae38a920657941a45ceb67737703b0d101a897b6
HaeSeon <gildogi@naver.com>
2020-08-20 17:51:04 +0900

    playlist update

--- app/src/main/java/com/example/music_recomend_profile/player/PlayList.kt
+++ app/src/main/java/com/example/music_recomend_profile/player/PlayList.kt
@@ -1,24 +1,29 @@
 package com.example.music_recomend_profile.player
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.music_recomend_profile.R
 import com.example.music_recomend_profile.TimeUtils
 import com.example.music_recomend_profile.database.DataExample
-import kotlinx.android.synthetic.main.activity_play_list.*
 import kotlin.properties.Delegates
 
 class PlayList : Fragment() {
 
+    private lateinit var callback: OnBackPressedCallback
     private lateinit var listRV: RecyclerView
+    private lateinit var dateTV : TextView
     private var position by Delegates.notNull<Int>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,13 +31,14 @@ class PlayList : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.activity_play_list, container, false)
         listRV = view.findViewById(R.id.playlistRV)
+        dateTV = view.findViewById(R.id.dateTextView)
 
 
         activity!!.intent.extras?.getInt("position")?.let {
             position = it
         }
 
-        dateTextView.text = DataExample().createRecordItem()[position].date?.let {
+        dateTV.text = DataExample().createRecordItem()[position].date?.let {
             TimeUtils().toDateString(
                 it
             )
@@ -47,5 +53,24 @@ class PlayList : Fragment() {
 
         return view
     }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val fragmentManager: FragmentManager = activity!!.supportFragmentManager
+                fragmentManager.beginTransaction().remove(this@PlayList).commit()
+                fragmentManager.popBackStack()
+
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
+    }
+
 
 }
