5dd1d2af1289f52ea9e1ed56fa4b82436f08b322
HaeSeon <gildogi@naver.com>
2020-08-20 19:18:35 +0900

    playlist design update

--- app/src/main/java/com/example/music_recomend_profile/player/PlayList.kt
+++ app/src/main/java/com/example/music_recomend_profile/player/PlayList.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -54,6 +53,7 @@ class PlayList : Fragment() {
         return view
     }
 
+    //fragment 생명주기 이용하여 backPressed 구현
     override fun onAttach(context: Context) {
         super.onAttach(context)
         callback = object : OnBackPressedCallback(true) {
